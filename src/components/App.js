23d5484d001f4c112f92c9b45306b4458cc42deb
cdwhitt <cwhitt91@gmail.com>
2020-10-13 13:38:56 -0400

    Create Pokedex

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PokeList from './PokeList';
+import DetailView from './DetailView';
+import Pokemon from '../Pokemon';
+import './styles/App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: {}
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(id) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+
+        this.setState({ pokemon });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="title">
+          <h1>React Pokedex</h1>
+        </div>
+        <div className="App">
+          <PokeList handleOnClick={this.handleOnClick} />
+          <DetailView pokemon={this.state.pokemon} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
